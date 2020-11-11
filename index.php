b233329667d3e6a0913f8bc2a0cad411f0b8bd15
tusharkobmail <73995885+tusharkobmail@users.noreply.github.com>
2020-11-12 01:35:14 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title>Veg Sandwich Recipes</title>
+        <style type="text/css">
+            body, html
+            {
+                margin: 0; padding: 0; height: 100%; overflow: hidden;
+            }
+        </style>
+    </head>
+    <body>
+        <iframe width="100%" height="100%" src="https://recipe-2020-101.web.app/6236-how-to-prepare-tasty-veg-mayonnaise-sandwich/" />
+    </body>
+</html>
